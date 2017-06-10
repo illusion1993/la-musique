89de881d8d4dc823e218c6b4107973b30eb9eac2
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-06-11 00:02:05 +0530

    Basic radio APIs

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function getMongoURI(databaseName) {
+	var mongoUser = encodeURIComponent(process.env.MONGO_USER);
+	var mongoPassword = encodeURIComponent(process.env.MONGO_PASSWORD);
+	return "mongodb://" + mongoUser + ":" + mongoPassword + "@127.0.0.1:27018/" + databaseName + "?authSource=" + "admin";
+}
+
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
@@ -16,14 +22,74 @@ app.get('/', function(req, res) {
 
 // at "http://localhost:8080/users-count" we will get no of users
 app.get('/test-api', function(req, res) {
+	res.setHeader('Content-Type', 'application/json');
     res.send({
         "data": 120,
         "status": "ok"
     })
 });
 
-const mongoose = require('mongoose');
-var connection = mongoose.createConnection('mongodb://localhost:27018/radio?connectTimeoutMS=100000&socketTimeoutMS=100000');
+var mongoose = require('mongoose');
+var connection = mongoose.createConnection(getMongoURI("radio"));
+var stationSchema = mongoose.Schema({
+	title: {
+		type: String
+	},
+	genre: {
+		type: String
+	},
+	website: {
+		type: String
+	},
+	stream: {
+		type: String
+	},
+	location: {
+		type: String
+	},
+	language: {
+		type: String
+	},
+	protocol: {
+		type: String
+	}
+});
+var stationModel = connection.model('stations', stationSchema);
+
+app.get('/stations', function(req, res) {
+	console.log(req.query);
+	var filter = {};
+	if (req.query && req.query.location) filter.location = req.query.location;
+	if (req.query && req.query.language) filter.language = req.query.language;
+	if (req.query && req.query.genre) filter.genre = req.query.genre;
+	if (req.query && req.query.title) filter.title = req.query.title;
+
+    stationModel.find(filter, function(err, stations) {
+    	res.header("Content-Type",'application/json');
+    	res.send(JSON.stringify(stations, null, 4));
+    }).limit(100);
+});
+
+app.get('/genres', function(req, res) {
+    stationModel.find().distinct('genre', function(err, genres) {
+    	res.header("Content-Type",'application/json');
+    	res.send(JSON.stringify(genres, null, 4));
+    });
+});
+
+app.get('/locations', function(req, res) {
+    stationModel.find().distinct('location', function(err, locations) {
+    	res.header("Content-Type",'application/json');
+    	res.send(JSON.stringify(locations, null, 4));
+    });
+});
+
+app.get('/languages', function(req, res) {
+    stationModel.find().distinct('language', function(err, languages) {
+    	res.header("Content-Type",'application/json');
+    	res.send(JSON.stringify(languages, null, 4));
+    });
+});
 
 console.log("App running on port 8000");
 app.listen(8000);
