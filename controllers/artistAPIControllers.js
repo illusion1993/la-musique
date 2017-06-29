3a1cf2a1d61137a75cf880b2e2124c15d1c9eb0d
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-06-30 04:43:06 +0530

    Artists API pagination done using mongoose-paginate package

--- controllers/artistAPIControllers.js
+++ controllers/artistAPIControllers.js
@@ -1,8 +1,9 @@
 var artistModel = require('../models/artistModel');
 
 exports.getArtists = function(req, res) {
+	var page_number = (req.query && req.query.page && (req.query.page == parseInt(req.query.page))) ? req.query.page - 1 : 0;
 	artistModel.getArtists(function(artists) {
 		res.header("Content-Type",'application/json');
 		res.json(artists);
-	});
+	}, page_number);
 }
--- models/artistModel.js
+++ models/artistModel.js
@@ -1,12 +1,21 @@
 var mongoose = require('mongoose');
 var dbConnection = require('../db/discogs');
+var mongoosePaginate = require('mongoose-paginate');
 
 var artistSchema = mongoose.Schema({
 	name: { type: String },
 });
+artistSchema.plugin(mongoosePaginate);
 
 var Artist = module.exports = dbConnection.model('artists', artistSchema);
 
-module.exports.getArtists = function(callback) {
-	callback([]);
+module.exports.getArtists = function(callback, page_number) {
+	Artist.paginate({}, { page: page_number + 1, limit: 20 }, function(err, result) {
+		callback({
+			results: result.docs,
+			page_number: result.page,
+			total_pages: result.pages,
+			total_items: result.total
+		});
+	});
 }
